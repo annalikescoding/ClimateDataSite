f799073b61fae894fcd3158087fb9892e82f95b8
annalikescoding <annalac2139@gmail.com>
2026-07-14 11:18:42 -0700

    add google maps

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-/* .js files add interaction to your website */
-// Map initialization
-var map = L.map('map').setView([51.505, -0.09], 13);
+//import "./style.css";
 
-//OpenStreetMap tile layer
-var openStreetMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// Map initialization
+var map = L.map('map').setView([51.505, -0.09], 3);
+
+//Google map tile layer
+
+googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
 });
-openStreetMap.addTo(map);
+googleStreets.addTo(map);
