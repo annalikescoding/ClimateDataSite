08726787e03704d182452d7b91d3fea2acc03913
annalikescoding <annalac2139@gmail.com>
2026-07-14 10:15:15 -0700

    added base and map

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
 /* .js files add interaction to your website */
-var reducewaylist = [
-  "Compost at home! You can use food waste to create nutrient-rich soil.",
-  "Buy the amount of food you need, not want.",
-  "Store food properly to prevent spoilage.",
-  "Plan meals for the week and only buy the ingredients you need.",
-  "Do not have a clustered fridge! If you do use up the food in your fridge before it spoils.",
-  "Sort your food waste into the compost bin!",
-];
+// Map initialization
+var map = L.map('map').setView([51.505, -0.09], 13);
 
-console.log(reducewaylist);
-
-var reduceway = document.getElementById("reduceway");
-var button1 = document.getElementById("button1");
-var count = 0;
-
-button1.addEventListener("click", displayReduceWay);
-
-function displayReduceWay(){
-  reduceway.innerHTML = reducewaylist[count];
-  count++;
-  if (count == reducewaylist.length){
-    count = 0;
-  }
-}
+//OpenStreetMap tile layer
+var openStreetMap = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+openStreetMap.addTo(map);
